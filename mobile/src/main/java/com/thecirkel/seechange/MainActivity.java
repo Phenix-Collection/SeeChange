7d884a97d1266cb0a58afc446e2c43eeb33e9d90
Kayvon Rahimi Morad Ali <krahimim@avans.nl>
2018-06-07 13:02:17 +0200

    Preparing socketconnection

--- mobile/src/main/java/com/thecirkel/seechange/MainActivity.java
+++ mobile/src/main/java/com/thecirkel/seechange/MainActivity.java
@@ -2,12 +2,33 @@ package com.thecirkel.seechange;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button connectBtn;
+    private TextView statusHeaderText, statusText;
+    private Connector connector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        connectBtn = (Button) findViewById(R.id.connectBtn);
+            connectBtn.setOnClickListener(connectBtnListener);
+        statusHeaderText = findViewById(R.id.statusHeaderText);
+        statusText = findViewById(R.id.statusText);
     }
+
+    private View.OnClickListener connectBtnListener = new View.OnClickListener(){
+        public void onClick(View v){
+//            connectBtn.setVisibility(View.INVISIBLE);
+            connector = new Connector(); //New instance of NetworkTask
+            connector.execute();
+            statusText.setText("Verbinden...");
+        }
+    };
+
 }
