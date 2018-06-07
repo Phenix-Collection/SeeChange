39913c9ff604540afbc386cf062c389c6a30a85d
Bart in 't Veld <bart.int.007@gmail.com>
2018-06-07 13:28:56 +0200

    Proof of concept made. Showing that data sharing is possible

--- mobile/src/main/java/com/thecirkel/seechange/MainActivity.java
+++ mobile/src/main/java/com/thecirkel/seechange/MainActivity.java
@@ -2,12 +2,44 @@ package com.thecirkel.seechange;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.wearable.DataClient;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String NUMBER_KEY = "com.seechange.key.number";
+    private DataClient dataClient;
+    private int number = 0;
+
+    private TextView numberView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numberView = findViewById(R.id.number);
+        numberView.setText(number + "");
+
+        dataClient = Wearable.getDataClient(this);
+    }
+
+    public void plusOne(View v) {
+        this.number++;
+        this.numberView.setText(number + "");
+
+        PutDataMapRequest putDataMapRequest = PutDataMapRequest.create("/number");
+        putDataMapRequest.getDataMap().putInt(NUMBER_KEY, number);
+
+        PutDataRequest putDataRequest = putDataMapRequest.asPutDataRequest();
+        putDataRequest.setUrgent();
+        dataClient.putDataItem(putDataRequest);
     }
 }
--- wear/src/main/java/com/thecirkel/seechange/MainActivity.java
+++ wear/src/main/java/com/thecirkel/seechange/MainActivity.java
@@ -1,21 +1,66 @@
 package com.thecirkel.seechange;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
 
-public class MainActivity extends WearableActivity {
+import com.google.android.gms.wearable.DataClient;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Wearable;
 
-    private TextView mTextView;
+public class MainActivity extends WearableActivity implements DataClient.OnDataChangedListener {
+
+    private static final String NUMBER_KEY = "com.seechange.key.number";
+    private int number = 0;
+
+    private TextView numberView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        numberView = findViewById(R.id.number);
+        numberView.setText(number + "");
 
         // Enables Always-on
         setAmbientEnabled();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Wearable.getDataClient(this).addListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Wearable.getDataClient(this).removeListener(this);
+    }
+
+    @Override
+    public void onDataChanged(@NonNull DataEventBuffer dataEvents) {
+        for (DataEvent event : dataEvents) {
+            if (event.getType() == DataEvent.TYPE_CHANGED) {
+                DataItem item = event.getDataItem();
+                if (item.getUri().getPath().compareTo("/number") == 0) {
+                    DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                    updateNumber(dataMap.getInt(NUMBER_KEY));
+                }
+            }
+        }
+    }
+
+    private void updateNumber(int number) {
+        this.number = number;
+        numberView.setText(number + "");
+    }
+
+
 }
