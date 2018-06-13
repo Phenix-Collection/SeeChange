a9a0f71740f2c6031cf09fab2d0b7f31683303a0
Kayvon Rahimi Morad Ali <krahimim@avans.nl>
2018-06-13 14:56:56 +0200

    added join/leave and send message actions

---
+++ mobile/src/main/java/com/thecirkel/seechange/Constants.java
@@ -0,0 +1,5 @@
+package com.thecirkel.seechange;
+
+public class Constants {
+    public static final String CHAT_SERVER_URL = "http://188.166.127.54:4200";
+}
--- mobile/src/main/java/com/thecirkel/seechange/MainActivity.java
+++ mobile/src/main/java/com/thecirkel/seechange/MainActivity.java
@@ -2,33 +2,155 @@ package com.thecirkel.seechange;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
 public class MainActivity extends AppCompatActivity {
-    private Button connectBtn;
-    private TextView statusHeaderText, statusText;
-    private Connector connector;
+    private static final String TAG = "MainActivity";
+
+    private Button connectBtn, disconnectBtn, sendMessageBtn;
+    private EditText messageText;
+    private TextView statusText;
+    private Socket mSocket;
+
+    private Boolean isConnected = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        connectBtn = (Button) findViewById(R.id.connectBtn);
-            connectBtn.setOnClickListener(connectBtnListener);
-        statusHeaderText = findViewById(R.id.statusHeaderText);
+        ChatApplication app = new ChatApplication();
+        mSocket = app.getSocket();
+        mSocket.on(Socket.EVENT_CONNECT, onConnect);
+        mSocket.on(Socket.EVENT_DISCONNECT,onDisconnect);
+        mSocket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
+        mSocket.on(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
+
         statusText = findViewById(R.id.statusText);
+        messageText = findViewById(R.id.messageText);
+
+        connectBtn = findViewById(R.id.connectBtn);
+        connectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSocket.connect();
+            }
+        });
+
+        disconnectBtn = findViewById(R.id.disconnectBtn);
+        disconnectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSocket.disconnect();
+            }
+        });
+
+        sendMessageBtn = findViewById(R.id.sendMessageBtn);
+        sendMessageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptSend();
+            }
+        });
     }
 
-    private View.OnClickListener connectBtnListener = new View.OnClickListener(){
-        public void onClick(View v){
-//            connectBtn.setVisibility(View.INVISIBLE);
-            connector = new Connector(); //New instance of NetworkTask
-            connector.execute();
-            statusText.setText("Verbinden...");
+    private void attemptSend() {
+        if (!mSocket.connected()) return;
+
+        String message = messageText.getText().toString().trim();
+        if (TextUtils.isEmpty(message)) {
+            messageText.requestFocus();
+            return;
+        }
+
+        Log.i(TAG, message);
+
+        messageText.setText("");
+        // perform the sending message attempt.
+
+        JSONObject data = new JSONObject();
+        try {
+            data.put("message", message);
+            data.put("username", "kayvon");
+            data.put("room", "room 1");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        mSocket.emit("chat_message", data);
+    }
+
+    private Emitter.Listener onConnect = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(!isConnected) {
+                        Log.i(TAG, "connected");
+                        statusText.setText("Connected");
+
+                        JSONObject data = new JSONObject();
+                        try {
+                            data.put("room", "room 1");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        mSocket.emit("join_room", data);
+                        isConnected = true;
+                    }
+                }
+            });
         }
     };
 
+    private Emitter.Listener onConnectError = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.e(TAG, "Error connecting");
+                    statusText.setText("Connection error");
+                }
+            });
+        }
+    };
+
+    private Emitter.Listener onDisconnect = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i(TAG, "disconnected");
+                    statusText.setText("Disconnected");
+                    isConnected = false;
+                }
+            });
+        }
+    };
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mSocket.disconnect();
+        mSocket.off(Socket.EVENT_CONNECT, onConnect);
+        mSocket.off(Socket.EVENT_DISCONNECT, onDisconnect);
+        mSocket.off(Socket.EVENT_CONNECT_ERROR, onConnectError);
+    }
 }
