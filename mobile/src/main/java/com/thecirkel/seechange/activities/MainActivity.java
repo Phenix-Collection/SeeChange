a111876d9112151dddd8a1697d6aecb822484525
Max Stubbe <mcj.stubbe@student.avans.nl>
2018-06-15 13:06:14 +0200

    bugfix prompts

--- mobile/src/main/java/com/thecirkel/seechange/activities/MainActivity.java
+++ mobile/src/main/java/com/thecirkel/seechange/activities/MainActivity.java
@@ -1,15 +1,14 @@
 package com.thecirkel.seechange.activities;
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.pm.PackageManager;
-import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.JsonWriter;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -24,45 +23,28 @@ import com.thecirkel.seechange.R;
 
 import net.ossrs.rtmp.ConnectCheckerRtmp;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.KeyStore;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.KeyManager;
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-
-import io.socket.client.IO;
-
 public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback, ConnectCheckerRtmp {
-
-
     private RtmpCamera1 camera;
     private SurfaceView cameraPreview;
     private Fragment chatFragment;
 
     private TextView liveText;
-    private ImageView recordButton;
-    private String PRIVATEKEY;
+    private ImageView recordButton, switchcameraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         recordButton = findViewById(R.id.recordButton);
+        switchcameraButton = findViewById(R.id.switchCameraButton);
+        switchcameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchCamera(view);
+            }
+        });
 
         liveText = findViewById(R.id.liveText);
         liveText.setVisibility(View.INVISIBLE);
@@ -72,48 +54,61 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
 
         cameraPreview = findViewById(R.id.cameraView);
 
-
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
             camera = new RtmpCamera1(cameraPreview, this);
             cameraPreview.getHolder().addCallback(this);
-        }else{
-            CheckPermissions();
+        } else {
+            requestPermissions();
         }
-
     }
 
-    public void CheckPermissions(){
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                // Permission is not granted
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+    public void requestPermissions(){
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+    }
+
+    Thread thread = new Thread(){
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(3500);
+                closeNow();
+            } catch (Exception  e) {
+                e.printStackTrace();
             }
-    }
+        }
+    };
 
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case 1: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // permission was granted, yay! Do the
                     // camera-related task you need to do.
-                    System.out.println("PERMISSION GRANTED");
                     camera = new RtmpCamera1(cameraPreview, this);
                     cameraPreview.getHolder().addCallback(this);
                     camera.startPreview();
+
+                    return;
                 } else {
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
-                    CheckPermissions();
+                    recordButton.setEnabled(false);
+                    switchcameraButton.setEnabled(false);
+                    Toast.makeText(this, "Can't stream without camera", Toast.LENGTH_SHORT).show();
+                    thread.start();
+                    return;
                 }
-                return;
             }
         }
-            // other 'case' lines to check for other
-            // permissions this app might request.
     }
 
-
+    private void closeNow() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            finishAffinity();
+        } else {
+            finish();
+        }
+    }
 
     public void goToChat(View v) {
         chatFragment.getView().setVisibility(View.VISIBLE);
